b64fa8823b9c6cfe51a98374b40b192fbe649988
JOSEPH <abdurahmonovabrorbek2023@gmail.com>
2025-09-22 14:52:22 +0900

    feat: develop author portfolio page

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
         console.log("ERROR:", err);
       } else{
-        user = JSON.parse(data)
+        user = JSON.parse(data) 
       }
-});
-
-//1: Kirish kodlari
+});  
+ 
+//1: Kirish kodlari 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//2: Session kodlari
+//2: Session kodlari 
 
+  
 //3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -32,15 +33,14 @@ res.json ({test: "succes"});
 
 app.get("/author", function(req, res ) {
     res.render("author", {user: user });
-});
+}); 
 
 app.get("/", function(req, res ) {
     res.render("harid");
 });
 
-
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running succesfully on port: ${PORT}`);
-});
+}); 
