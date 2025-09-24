6393e07b865a3829e5415988dbdabcfbbe5fc97d
JOSEPH <abdurahmonovabrorbek2023@gmail.com>
2025-09-24 20:02:23 +0900

    feat: MongoDB yordamida reja application qurish

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
-console.log ("Web serverni boshlash");
-const express = require("express");
+console.log('Web Serverni Boshlash');
+const express = require('express');
 const app = express();
-const fs = require("fs");
-const res = require("express/lib/response")
+const fs = require('fs');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-      } else{
-        user = JSON.parse(data) 
-      }
-});  
- 
-// MongoDB chaqirish
-const db = require ("./server").db();
+// Mongo DB choqirish
+const db = require('./server').db(); 
 
-//1: Kirish kodlari 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
-//2: Session kodlari   
+
+//1 Kirish/ MIDDLEWARE
+app.use(express.static('public')); // DP  public ochiqlaydi
+app.use(express.json()); // Midleware DP -> Rest API 
+app.use(express.urlencoded({ extended: true }));
+
+
+//2 Session
+
 //3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4: Routing code
+//4 Routing code
 app.post("/create-item", (req, res) => {
-  console.log('user entered /create')
-console.log(req.body);
-const new_reja = req.body.reja;
-res.json ({test: "succes"});
-db.collection("plans").insertOne({reja: new_reja }, (err, data) =>{
-  if (err) { 
-    console.log(err);
-    res.end("something went wrong");
-    } else {
-      res.end("succesfully added");
-    }  
+     console.log('user entered /create-item');
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end('something went wrong')
+        } else {
+            res.end('successfully addes ')
+        }
+    });
 });
 
+app.get("/hello", function(req, res) {
+    res.end(`<h1 style="background: yellowgreen">Hello World by Azamat</h1>`);
+})
+console.log("A");
+app.get("/portfolio", (req, res) => {
+    console.log("B");
+    res.render("portfolio", { user: user });
+});
+console.log("C");
+
+app.get("/", function(req, res) {
+    console.log('user entered /');
+    db.collection("plans").find().toArray((err, data) => {
+       if(err) {
+        console.log(err);
+        req.end("Something went wrong");
+       } else {
+        console.log(data);
+         res.render("reja", {items: data});
+       }
+    })
 });
 
-app.get("/author", function(req, res ) {
-    res.render("author", {user: user });
-}); 
 
-app.get("/", function(req, res ) {
-  console.log('user entered /')
-  db.collection("plans").find().toArray ((err, data) => {
-    if(err){
-      console.log(err);
-      res.end(("something went wrong"));      
-    } else{
-      console.log(data);
-       res.render("reja", { items: data });
-    }
-  })
-   
-});
 
 module.exports = app;
-
