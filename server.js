af8f74581f1d57fb4ddcc1a0b826c9ad156ad680
JOSEPH <abdurahmonovabrorbek2023@gmail.com>
2025-10-03 15:35:10 +0900

    fix: Reja loyihamizni yakuniy bosqich logic

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongodb.connect(connectionString, {
   
     const app = require("./app");
     const server = http.createServer(app);
-    let PORT = 4006;
+    let PORT = 3000;
     server.listen(PORT, function() {
     console.log(
         `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
